55fce5096bf2a232f593aec603e5021e9f819048
Sheldon Perkins <csperkins18@gmail.com>
2019-08-09 20:51:52 -0400

    <button 1 for red random value>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
+// Variable declarations
 var wins= 0;
 var losses= 0;
 var userScore= 0;
 
-
+// Button values Math.floor(Math.random() * ((y-x)+1) + x);
+var button1= math.floor(Math.random() * ((120 - 19)+1)+ 19);
 });
