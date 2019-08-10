e28dc50cd7a0aaa78fb81caedadee4e6e9a26afd
Sheldon Perkins <csperkins18@gmail.com>
2019-08-09 23:00:02 -0400

    <click events and target score>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,11 @@ var wins= 0;
 var losses= 0;
 var userScore= 0;
 
+
 // Button values Math.floor(Math.random() * ((y-x)+1) + x);
 // target score
 var targetScore= Math.floor(Math.random() * ((120 - 19)+1)+ 19);
+document.getElementById("#targetScore").text(targetScore);
 // Crystal values
 var button1= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
 var button2= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
@@ -14,9 +16,20 @@ var button3= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
 var button4= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
 // click event
 $("#imageR").click(function(){
-    alert("The paragraph was clicked.");
+    
   });
+  $("#imageW").click(function(){
+    
+});
+$("#imageB").click(function(){
+    
+});
+$("#imageBL").click(function(){
+    
+});
 // if win
 
 // if lose
+
+// user score
 });
