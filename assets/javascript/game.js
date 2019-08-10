1a1ec05f15227a430e8486f4302eb3d39b9aa67b
Sheldon Perkins <csperkins18@gmail.com>
2019-08-09 21:18:01 -0400

    <target score and chrystal button values>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,5 +5,11 @@ var losses= 0;
 var userScore= 0;
 
 // Button values Math.floor(Math.random() * ((y-x)+1) + x);
-var button1= math.floor(Math.random() * ((120 - 19)+1)+ 19);
+// target score
+var targetScore= math.floor(Math.random() * ((120 - 19)+1)+ 19);
+// Crystal values
+var button1= math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button2= math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button3= math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button4= math.floor(Math.random() * ((12 - 1)+1)+ 1);
 });
