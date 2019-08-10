d09c802d49f3a33cc1b1f4debe264f25432a1468
Sheldon Perkins <csperkins18@gmail.com>
2019-08-09 22:25:46 -0400

    <Math syntax fix>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,16 @@ var userScore= 0;
 
 // Button values Math.floor(Math.random() * ((y-x)+1) + x);
 // target score
-var targetScore= math.floor(Math.random() * ((120 - 19)+1)+ 19);
+var targetScore= Math.floor(Math.random() * ((120 - 19)+1)+ 19);
 // Crystal values
-var button1= math.floor(Math.random() * ((12 - 1)+1)+ 1);
-var button2= math.floor(Math.random() * ((12 - 1)+1)+ 1);
-var button3= math.floor(Math.random() * ((12 - 1)+1)+ 1);
-var button4= math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button1= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button2= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button3= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
+var button4= Math.floor(Math.random() * ((12 - 1)+1)+ 1);
 // click event
-
+$("#imageR").click(function(){
+    alert("The paragraph was clicked.");
+  });
 // if win
 
 // if lose
