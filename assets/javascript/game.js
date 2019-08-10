d338f7c4df83b1132e79aa2ea48763457c87f8c2
Sheldon Perkins <csperkins18@gmail.com>
2019-08-10 01:55:29 -0400

    <css>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
   var userScore = 0;
 
 
-  // Button values Math.floor(Math.random() * ((y-x)+1) + x);
+  // Target & Button values Math.floor(Math.random() * ((y-x)+1) + x);
   // target score
   var targetScore = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-  // document.getElementById("#targetScore").text(targetScore);
   $("#targetScore").text(targetScore);
+
   // Crystal values
   var button1 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
   var button2 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
@@ -17,40 +17,38 @@ $(document).ready(function () {
   var button4 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
 
   // click events
-  if (targetScore > userScore) {
+  // if (targetScore !== userScore) {
+
     $("#imageR").click(function () {
       userScore += button1;
       $("#userScore").html(userScore);
-      console.log(button1);
-        });
+    });
     $("#imageW").click(function () {
       userScore += button2;
       $("#userScore").html(userScore);
-      console.log(button2);
     });
     $("#imageB").click(function () {
       userScore += button3;
       $("#userScore").html(userScore);
-      console.log(button3);
     });
     $("#imageBl").click(function () {
       userScore += button4;
       $("#userScore").html(userScore);
-      console.log(button4);
     });
+  // };
+  // if win
+  if (userScore === targetScore) {
+    wins++
+    $("#win").html(wins);
+    alert("You Win!")
+    console.log(wins)
+  };
 
-    // if win
-    if (userScore === targetScore) {
-      wins++
-      $("#win").html(wins);
-      alert("You Win!")
-    }
-
-    // if lose
-    if (userScore > targetScore) {
-      losses++
-      $("#lose").html(losses);
-      alert("You lose!");
-    }
+  // if lose
+  if (userScore > targetScore) {
+    losses++
+    $("#lose").html(losses);
+    alert("You lose!");
+    console.log(losses)
   };
 });
